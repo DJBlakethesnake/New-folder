2c8d63b948d6a2fe0aec50ec5d37d1aea2d0a30e
DJBlakethesnake <djblakethesnake1@gmail.com>
2026-05-31 21:18:25 -0400

    Refactor app creation and configuration
    Refactor create_app function to use environment variables for configuration and improve instance path handling.

--- website/__init__.py
+++ website/__init__.py
@@ -1,31 +1,34 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app = Flask(__name__, instance_relative_config=True)
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-only-change-this")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(app.instance_path, DB_NAME)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
 
     from .views import views
     from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User
-    
+
     with app.app_context():
         db.create_all()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -33,9 +36,3 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
